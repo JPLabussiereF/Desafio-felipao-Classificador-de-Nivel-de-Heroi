2c35c924affd5866d8d8c2788310fd2d3ad8c102
JPLabussiereF <labussework@gmail.com>
2024-02-20 21:19:19 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-let myN = João;
-console.log("Oii");
+let nomeHeroi = "Labussiere";
+let XP = 900;
+let nivel;
+
+switch (XP){
+    case XP<1000:
+    nivel = ferro;
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel);
+    break;
+}
