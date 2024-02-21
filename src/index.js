6e8c54314bcebb656c6147de0bee96a87e0b2c54
JPLabussiereF <labussework@gmail.com>
2024-02-20 22:58:21 -0300

    Desafio Finalizado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-let nomeHeroi = "Labussiere";
-let XP = 900;
+   //Usar o true dentro do swtich para esses casos 
+
+let nomeHeroi = "Labussiere";  
+let xp = 9100;
 let nivel;
 
-switch (XP){
-    case XP<1000:
-    nivel = ferro;
-    console.log("O Herói " + nomeHeroi + " está no nível " + nivel);
+ switch (true){ //Usar o true dentro do swtich para esses casos
+    
+    case xp < 1001:
+    nivel = "Ferro";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
     break;
+
+    case xp <= 2000:
+    nivel = "Bronze";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
+    case xp <= 5000:
+    nivel = "Prata";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
+    case xp <= 7000:
+    nivel = "Ouro";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+   
+
+    case xp <= 8000:
+    nivel = "Platina";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
+    case xp <= 9000:
+    nivel = "Ascendente";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
+    case xp <= 10000:
+    nivel = "Imortal";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
+    default: 
+    nivel = "Radiante";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivel + "!");
+    break;
+
 }
